24ddf6ae3dc3941add11a041170e80a6930fd06e
lazkopat3522 <ikramamzkg@gmail.com>
2025-04-04 11:28:59 -0400

    Add files via upload

---
+++ bsf .java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n;
+
+        n = input.nextInt();
+        System.out.println(n%10);
+    }
+}
