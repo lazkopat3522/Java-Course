d8e38e49f95d042fe910870e6f16f63de53cb158
lazkopat3522 <ikramamzkg@gmail.com>
2025-04-16 00:27:58 -0400

    Array_M

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=sc.nextInt();
+
+
+
+    }
+}
