200781bece6633dd294d4ce999e8cffdf09b5e9d
lazkopat3522 <ikramamzkg@gmail.com>
2025-04-16 19:58:26 -0400

    char

---
+++ src/Leet_1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Leet_1 {
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        char ch='a';
+        char ch1='\u0061';
+        char ch2=97;
+        System.out.print("My values are "+ch+ch1+ch2);
+
+
+
+
+    }
+}
